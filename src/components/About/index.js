4870734be5e4e8f2d686d200347310fd936cb64b
Alvin Yearby <leroy.yearby@gmail.com>
2022-03-06 16:25:12 -0500

    edited about me

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+
+function About() {
+    return (
+        <section class="about">
+            <h1 id="about">About Me</h1>
+            <p>Welcome to my portfolio! Im a recent college grad with a degree in Business Administration. Born and raised on military bases across South Korea, I am what you would call a “military brat.” I currently reside in North Carolina and am an avid Tar Heels fan. In my free time, I enjoy traveling, reading and playing games. 
+                I’m hoping to obtain a career in Web Development upon completing this bootcamp.</p>
+        </section>
+    ); 
+}
+
+export default About; 
