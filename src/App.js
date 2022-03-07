63953399d074f6d2d40fcded8334fd58c185cc6c
Alvin Yearby <leroy.yearby@gmail.com>
2022-03-06 22:48:18 -0500

    edited app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+ 
 import About from './components/About';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <main>
-          <About></About>
-        </main>
-      </header>
-    </div>
-  );
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import Portfolio from './components/Portfolio';
+ 
+class App extends Component {
+ render() {
+   return (
+     <HashRouter basename="/">
+       <div className="app">
+         <Header />
+         <main>
+         <Routes>
+           <Route path="/" element= { <Home/> } />
+           <Route path="/about" element={ <About/> } />
+           <Route path="/contact" element= { <Contact/> } />
+           <Route path="/portfolio" element={ <Portfolio/> } />
+         </ Routes>
+         </main>
+         <Footer />
+       </div>
+     </HashRouter>
+   );
+ }
 }
-
+ 
 export default App;
 
 
